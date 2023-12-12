6da7e4357074cdb4f4baa00d42e97e5d3252373a
EgorikBro <nazarenkoe106@gmail.com>
2023-12-12 15:09:35 +0500

    начало

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
+import os
+import sys
+
 import pygame
 
 pygame.init()
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.image = load_image('idle.png', -1)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rect.x -= 5
+        if keys[pygame.K_d]:
+            self.rect.x += 5
+        if keys[pygame.K_w]:
+            self.rect.y -= 5
+        if keys[pygame.K_s]:
+            self.rect.y += 5
+
+
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
 running = True
 FPS = 60
 clock = pygame.time.Clock()
@@ -11,6 +51,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill('white')
+    all_sprites.update()
+    screen.fill('black')
+    all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
